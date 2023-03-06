e240653c7332df3fcb23168e3c31304b0a37be7a
augustofaggion <afaggionliradossantos@gmail.com>
2023-03-06 07:20:04 +0100

    begin of game

--- game.js
+++ game.js
@@ -10,3 +10,34 @@ const getUserInput = (userInput) => {
         console.log('Error!');
     }
 }
+
+const getComputerChoice = () => {
+    const randomNumber = Math.floor(Math.random() * 3);
+    
+    switch(randomNumber) {
+        case 0 :
+            return 'rock';
+            break;
+        case 1:
+            return 'scissors';
+            break;
+        case 2:
+            return 'paper';
+            break;
+    }
+};
+
+const determineWinner = (userChoice, computerChoice) => {
+    if(userChoice === computerChoice){
+        return 'This game is a Tie';
+    }
+    if(userChoice === 'rock'){
+        if(computerChoice === 'paper'){
+            return 'Sorry you lost';
+        }
+    } else {
+        return 'Congratulations you won';
+    }
+}
+
+if
